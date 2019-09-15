ce44d3b4b96dbb6c42b594f84a1631b2743ffbfd
jjdiep <jjdiep@umich.edu>
2019-09-15 17:36:00 -0400

    HeapSort implemented

--- tutorial_heapsort/heap.js
+++ tutorial_heapsort/heap.js
@@ -27,12 +27,30 @@ minheaper = {};
 
 // define insert function for min binary heap
 function minheap_insert(heap, new_element) {
+  var elntIdx = heap.length;
+  var prntIdx = Math.floor( (elntIdx - 1)/ 2);
+
+  heap.push(new_element);
 
     // STENCIL: implement your min binary heap insert operation
+    var heaped = (elntIdx <= 0) || (heap[prntIdx] <= heap[elntIdx]);
+    while (!heaped) {
+      // Swap element and parent
+      var tmp = heap[prntIdx];
+      heap[prntIdx] = heap[elntIdx];
+      heap[elntIdx] = tmp;
+
+      // Update element and parent index
+      elntIdx = prntIdx;
+      prntIdx = Math.floor( (elntIdx -1 ) / 2 );
+
+      // Re-evaluate heap condition
+      heaped = (elntIdx <= 0) || (heap[prntIdx] <= heap[elntIdx]);
+    }
 }
 
 // assign insert function within minheaper object
-minheaper.insert = minheap_insert;
+      minheaper.insert = minheap_insert;
 /* Note: because the minheap_insert function is an object, we can assign 
       a reference to the function within the minheap object, which can be called
       as minheap.insert
@@ -41,13 +59,44 @@ minheaper.insert = minheap_insert;
 // define extract function for min binary heap
 function minheap_extract(heap) {
 
-    // STENCIL: implement your min binary heap extract operation
+    // // STENCIL: implement your min binary heap extract operation
+    if (heap.length == 1) {
+       return heap.pop();
+    }
+    // Extract root of heap, replace with farthest right node
+    var lastHeap = heap.pop();
+    var minHeap = heap[0];
+    heap[0] = lastHeap;
+    
+    // Heapify
+    var elntIdx = 0;
+    var heaped = 0; 
+
+    while (!heaped) {
+      leftHeap = 2 * elntIdx + 1;
+      rightHeap = 2 * elntIdx + 2;
+      var tmp = heap[elntIdx];
+      if (leftHeap >= heap.length) {
+        break;
+      } else if (((rightHeap >= heap.length) && (heap[elntIdx] > heap[leftHeap])) || ((rightHeap < heap.length) && (heap[elntIdx] > heap[leftHeap]) && (heap[leftHeap] <= heap[rightHeap]))) {
+        heap[elntIdx] = heap[leftHeap];
+        heap[leftHeap] = tmp;
+        elntIdx = leftHeap;
+      } else if ((rightHeap  < heap.length) && (heap[elntIdx] > heap[rightHeap]) && (heap[leftHeap] > heap[rightHeap])) {
+        heap[elntIdx] = heap[rightHeap];
+        heap[rightHeap] = tmp;
+        elntIdx = rightHeap;
+      } else {
+        break;
+      }
+    }
+  return minHeap;
 }
 
 // assign extract function within minheaper object
 
     // STENCIL: ensure extract method is within minheaper object
-    minheaper.extract = minheap_extract;
+      minheaper.extract = minheap_extract;
 
 
 
